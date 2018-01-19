31637cb2d392b7c07884e67b356ad16b325e792c
Irtaza Abbas <irtazabbas@gmail.com>
2018-01-19 11:43:42 +0500

    new - mw for generating auth token

--- authorization.js
+++ authorization.js
@@ -6,12 +6,15 @@ var mongoose = require('mongoose'),
 
 var findUser = exports.findUser = function(id, cb) {
   User.findOne({
-        _id: id
-    }, function(err, user) {
-        if (err || !user) return cb(null);
-        cb(user);
-    });
+    _id: id
+  }, function(err, user) {
+    if (err || !user) return cb(null);
+    cb(user);
+  });
 };
+
+const jwt = require('jsonwebtoken');
+const config = require('meanio').getConfig();
 
 
 /**
@@ -62,4 +65,35 @@ exports.isMongoId = function(req, res, next) {
       return res.status(500).send('Parameter passed is not a valid Mongo ObjectId');
   }
   next();
+};
+
+
+exports.generateAuthToken = function(MeanUser) {
+  return (req, res, next) => {
+    try {
+      let payload = req.user;
+      let escaped, token;
+
+      if (MeanUser) {
+        MeanUser.events.publish({
+          action: 'logged_in',
+          user: {
+            name: req.user.name
+          }
+        });
+      }
+
+      (req.body.hasOwnProperty('redirect') && req.body.redirect !== false) &&
+      (payload.redirect = req.body.redirect);
+
+      escaped = JSON.stringify(payload);
+      escaped = encodeURI(escaped);
+
+      req.token = jwt.sign(escaped, config.secret);
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  }
 };
